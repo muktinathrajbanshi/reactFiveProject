db504927a429ce5a2113bb54aa4a211bad2ac13a
unknown <muktinathrajbanshi786@gmail.com>
2025-07-01 23:35:13 +0545

    Api is created for my project

--- src/component/Basics/Resturant.js
+++ src/component/Basics/Resturant.js
@@ -9,7 +9,15 @@ const Resturant = () => {
             <div className="card-body">
                 <span className="card-number card-circle subtle">1</span>
                 <span className="card-author subtle">Breakfast</span>
+                <h2 className="card-title">Maggi</h2>
+                <span className="card-description subtle">
+                    Maggi is a quick, delicious instant noodle loved worldwide. Its signature masala flavor, easy preparation, and comforting warmth make it a go-to snack or meal. Perfect for late nights, cravings, or nostalgic childhood moments.
+                </span>
+                <div className="card-read"></div>
             </div>
+            <img src="" alt="" className="card-media" />
+
+            <span className="card-tag subtle">Order Now</span>
         </div>
       </div>
     </>
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,74 @@
+const Menu = [
+    {
+        id: 1,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 2,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 3,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 4,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 5,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 6,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 7,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 8,
+        image: "",
+        name: "maggi",
+        category: "lunch",
+        price: 25,
+        description: "maggi has fantastic taste"
+    },
+    {
+        id: 9,
+        image: "../images/somosa.png",
+        name: "samosa",
+        category: "evening",
+        price: "Rs.25",
+        description: "maggi has fantastic taste"
+    },
+];
