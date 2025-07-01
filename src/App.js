4fb48f2dd53600c5d3332c63ea81f11831564164
unknown <muktinathrajbanshi786@gmail.com>
2025-07-01 23:01:00 +0545

    my project started from restaurant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Resturant from './component/Basics/Resturant';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Muktinath, Welcome to new Future!</h1>
-    </div>
+    <>
+      <Resturant />
+    </>
   )
 }
 
