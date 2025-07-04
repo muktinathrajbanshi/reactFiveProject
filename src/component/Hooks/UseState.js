8f93fcd92be53dc8ef5828710deba331719a59e8
unknown <muktinathrajbanshi786@gmail.com>
2025-07-04 17:08:30 +0545

    my second project is completed

--- src/component/Hooks/UseState.js
+++ src/component/Hooks/UseState.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
 const UseState = () => {
+    
+    const[myNum, setMyNum] = useState(0);
+
   return (
     <>
         <div className="center_div">
-            <p>my data</p>
-            <div className="button2">
+            <p>{myNum}</p>
+            <div className="button2" onClick={() => setMyNum(myNum + 1)}>
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
                 INCR
             </div>
-            <div className="button2">
+            <div className="button2" onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
                 <span></span>
                 <span></span>
                 <span></span>
