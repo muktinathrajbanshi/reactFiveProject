cf5af2f18d22b7d7601c9928a04cfc1051507bfd
unknown <muktinathrajbanshi786@gmail.com>
2025-07-04 12:35:40 +0545

    my next project is started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Resturant from './component/Basics/Resturant';
+import UseState from "./component/Hooks/UseState";
 
 const App = () => {
   return (
     <>
-      <Resturant />
+      <UseState />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/component/Hooks/UseState.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./style.css";
+
+const UseState = () => {
+  return (
+    <>
+        <div className="center_div">
+            <p>my data</p>
+            <div className="button2">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                HOVER ME
+            </div>
+            <div className="button2">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                HOVER ME
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default UseState;
