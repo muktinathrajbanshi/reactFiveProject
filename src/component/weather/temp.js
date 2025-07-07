47bf7a17892365f107323a29640cb18c1dcdbdec
unknown <muktinathrajbanshi786@gmail.com>
2025-07-07 18:33:25 +0545

    my static webpage is created but css and dynamic is remain

--- src/component/weather/temp.js
+++ src/component/weather/temp.js
@@ -12,8 +12,70 @@ const Temp = () => {
                 id="search"
                 className="searchTerm"
               />
+              <button className="searchButton" type="button">
+                Search
+              </button>
         </div>
       </div>
+      {/* our temp card */}
+        <article className="width">
+            <div className="weatherIcon">
+                <i className={"wi wi-day-sunny"}></i>
+            </div>
+            <div className="weatherInfo">
+                <div className="temperature">
+                    <span>25.5&deg;</span>
+                </div>
+                <div className="description">
+                    <div className="weatherCondition">sunny </div>
+                    <div className="place">pune, India</div>
+                </div>
+            </div>
+            <div className="date"> { new Date().toLocaleDateString()} </div>
+            {/* our 4 column section */}
+            <div className="extra-temp">
+                <div className="temp-info-minmax">
+                    <div className="two-sided-section">
+                        <p>
+                        <i className={"wi wi-sunset"}></i>
+                        </p>
+                        <p className="extra-info-leftside">
+                        19:12 PM <br />
+                        Sunset
+                        </p>
+                    </div>
+                    <div className="two-sided-section">
+                        <p>
+                        <i className={"wi wi-humidity"}></i>
+                        </p>
+                        <p className="extra-info-leftside">
+                        19:12 PM <br />
+                        Humidity
+                        </p>
+                    </div>
+                </div>
+                <div className="weather-extra-info">
+                    <div className="two-sided-section">
+                        <p>
+                        <i className={"wi wi-rain"}></i>
+                        </p>
+                        <p className="extra-info-leftside">
+                        19:12 PM <br />
+                        Pressure
+                        </p>
+                    </div>
+                    <div className="two-sided-section">
+                        <p>
+                        <i className={"wi wi-strong-wind"}></i>
+                        </p>
+                        <p className="extra-info-leftside">
+                        19:12 PM <br />
+                        Speed
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </article>
     </>
   )
 }
