2c48c6bcbd1ec94c3336483d1beb0848ad2e2e95
unknown <muktinathrajbanshi786@gmail.com>
2025-07-08 21:38:21 +0545

    90% backend completed by using API, JS and React

--- src/component/weather/temp.js
+++ src/component/weather/temp.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import "./style.css";
+import Weathercard from './weathercard';
 
 const Temp = () => {
 
     const [searchValue, setSearchValue] = useState("pune");
+    const [tempInfo, setTempInfo] = useState({});
 
     const getWeatherInfo = async () => {
         try {
@@ -12,7 +14,25 @@ const Temp = () => {
             let res = await fetch(url);
             let data = await res.json();
 
-            console.log(data);
+
+            const {temp, humidity, pressure} = data.main;
+            const {main: weathermood } = data.weather[0];
+            const {name} = data;
+            const {speed} = data.wind;
+            const {country, sunset} = data.sys;
+
+            const myNewWeatherInfo = {
+                temp, 
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+
+            setTempInfo(myNewWeatherInfo);
 
         } catch (error) {
             console.log(error);
@@ -41,68 +61,7 @@ const Temp = () => {
         </div>
       </div>
       {/* our temp card */}
-        <article className="width">
-            <div className="weatherIcon">
-                <i className={"wi wi-day-sunny"}></i>
-            </div>
-            <div className="weatherInfovsDate">
-                <div className="weatherInfo">
-                    <div className="temperature">
-                        <span>25.5&deg;</span>
-                    </div>
-                    <div className="description">
-                        <div className="weatherCondition">sunny </div>
-                        <div className="place">pune, India</div>
-                    </div>
-                </div>
-                <div className="date">
-                    { new Date().toLocaleString()} 
-                </div>
-            </div>
-            {/* our 4 column section */}
-            <div className="extra-temp">
-                <div className="temp-info-minmax">
-                    <div className="two-sided-section">
-                        <p>
-                        <i className={"wi wi-sunset"}></i>
-                        </p>
-                        <p className="extra-info-leftside">
-                        19:12 PM <br />
-                        Sunset
-                        </p>
-                    </div>
-                    <div className="two-sided-section">
-                        <p>
-                        <i className={"wi wi-humidity"}></i>
-                        </p>
-                        <p className="extra-info-leftside">
-                        19:12 PM <br />
-                        Humidity
-                        </p>
-                    </div>
-                </div>
-                <div className="weather-extra-info">
-                    <div className="two-sided-section">
-                        <p>
-                        <i className={"wi wi-rain"}></i>
-                        </p>
-                        <p className="extra-info-leftside">
-                        19:12 PM <br />
-                        Pressure
-                        </p>
-                    </div>
-                    <div className="two-sided-section">
-                        <p>
-                        <i className={"wi wi-strong-wind"}></i>
-                        </p>
-                        <p className="extra-info-leftside">
-                        19:12 PM <br />
-                        Speed
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </article>
+        <Weathercard tempInfo={tempInfo}/>
     </>
   )
 }
